14dfe168334364c2871d742dad45f28b74812980
hungndhe150171 <hungndhe150171@fpt.edu.vn>
2024-05-19 04:56:41 +0700

    design home page
    design home page 30-40%

---
+++ web/JS/script.js
@@ -0,0 +1,14 @@
+document.getElementById('openFormBtn').addEventListener('click', function() {
+    document.getElementById('consultationForm').classList.add('show');
+});
+
+document.getElementById('closeFormBtn').addEventListener('click', function() {
+    document.getElementById('consultationForm').classList.remove('show');
+});
+
+function handleSubmit(event) {
+    event.preventDefault();
+    alert('Đăng ký thành công, chúng tôi sẽ sớm liên lạc qua email của bạn');
+    document.getElementById('form').reset();
+    document.getElementById('consultationForm').classList.remove('show');
+}
