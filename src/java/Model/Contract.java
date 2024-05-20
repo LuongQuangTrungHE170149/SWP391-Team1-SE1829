66a792cdd5a3c0db3e5a54e43806d744c4356ff5
LuongQuangTrungHE170149 <134850662+LuongQuangTrungHE170149@users.noreply.github.com>
2024-05-20 14:04:50 +0700

    Create contract.java

---
+++ src/java/Model/Contract.java
@@ -0,0 +1,87 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+import java.util.Date;
+
+/**
+ *
+ * @author QUANG TRUNG
+ */
+public class Contract {
+    private String contractId;
+    private String agencyId;
+    private Date startDate;
+    private Date endDate;
+    private String contractType;
+    private String descripttion;
+    private Double payment;
+    private String status;
+
+    public String getContractId() {
+        return contractId;
+    }
+
+    public void setContractId(String contractId) {
+        this.contractId = contractId;
+    }
+
+    public String getAgencyId() {
+        return agencyId;
+    }
+
+    public void setAgencyId(String agencyId) {
+        this.agencyId = agencyId;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getContractType() {
+        return contractType;
+    }
+
+    public void setContractType(String contractType) {
+        this.contractType = contractType;
+    }
+
+    public String getDescripttion() {
+        return descripttion;
+    }
+
+    public void setDescripttion(String descripttion) {
+        this.descripttion = descripttion;
+    }
+
+    public Double getPayment() {
+        return payment;
+    }
+
+    public void setPayment(Double payment) {
+        this.payment = payment;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    
+}
