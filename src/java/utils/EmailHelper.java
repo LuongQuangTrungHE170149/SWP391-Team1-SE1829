a739b247f71676224b4e630103cdc8d5cacba850
xxduog2702 <duongtxhe160708@gmail.com>
2024-05-22 00:41:36 +0700

    register User - get values from backend and validate

--- src/java/utils/EmailHelper.java
+++ src/java/utils/EmailHelper.java
@@ -1,9 +1,18 @@
-
 package utils;
 
 import java.util.Properties;
+import java.util.Random;
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
 public class EmailHelper {
+
     private static final String EMAIL_USERNAME = "xxx@gmail.com";
     private static final String EMAIL_PASSWORD = "xxx";
 
@@ -58,4 +67,3 @@ public class EmailHelper {
         return otp.toString();
     }
 }
- 
