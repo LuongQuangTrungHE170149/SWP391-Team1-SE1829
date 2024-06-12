2e387565b4b5c9b4118732bf423e2aa8834f0733
xxduog2702 <duongtxhe160708@fpt.edu.vn>
2024-06-13 03:33:28 +0700

    note

--- src/java/utils/StringUtils.java
+++ src/java/utils/StringUtils.java
@@ -9,11 +9,11 @@ package utils;
  * @author trand
  */
 public class StringUtils {
-
+//validate dữ liệu nhập vào 
     public static String REGEX_CHARACTER_AND_NUMBERS = "^[a-zA-Z0-9]+$";
     public static String REGEX_PHONE = "[0-9]{10,15}$";
     public static String REGEX_EMAIL = "^[a-zA-Z0-9._]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$";
-
+//đếm kí tự khi đã xóa dấu cách
     public static boolean isEmpty(String message) {
         return message == null || message.trim().length() == 0;
     }
@@ -21,7 +21,7 @@ public class StringUtils {
     public static boolean isMaxLength(String message) {
         return message.trim().length() < 0 || message.trim().length() > 255;
     }
-
+//không lấy kí tự đặc biệt
     public static boolean isOnlyCharacterAndNumber(String message) {
         return message.trim().matches(REGEX_CHARACTER_AND_NUMBERS);
     }
