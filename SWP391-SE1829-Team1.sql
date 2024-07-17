e9ac765b899bd96fe1ab76208536c860a3c8d1ef
Ductmhe160745 <ductmhe160745@fpt.edu.vn>
2024-07-17 08:41:20 +0100

    update

--- SWP391-SE1829-Team1.sql
+++ SWP391-SE1829-Team1.sql
@@ -54,6 +54,7 @@ create table Vehicles(
 			Chassis nvarchar(255),
 			Engine nvarchar(255),
 )
+
 create table Contracts(                                        /*bao hiem xe may cua Duong???*/
 			ContractId int identity(1,1) primary key,
 			CustomerId int foreign key references Users(id),
