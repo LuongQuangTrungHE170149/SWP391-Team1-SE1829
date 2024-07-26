63ea333c83c23c2ecf3138e401f7a61379d66c9a
LuongQuangTrungHE170149 <134850662+LuongQuangTrungHE170149@users.noreply.github.com>
2024-07-26 20:08:47 +0700

    Update validate

--- src/java/Controller/CheckLicensePlateServlet.java
+++ src/java/Controller/CheckLicensePlateServlet.java
@@ -2,12 +2,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
+
 package Controller;
 
-import Model.Contract;
-import Model.User;
-import dal.ContractDAO;
-import dto.Contractdto;
 import java.io.IOException;
 import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
@@ -15,48 +12,41 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.sql.SQLException;
-import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
  * @author QUANG TRUNG
  */
-@WebServlet(name = "ListContract", urlPatterns = {"/ListContract"})
-public class ListContract extends HttpServlet {
-
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
+@WebServlet(name="CheckLicensePlateServlet", urlPatterns={"/CheckLicensePlateServlet"})
+public class CheckLicensePlateServlet extends HttpServlet {
+   
+    /** 
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet ListContract</title>");
+            out.println("<title>Servlet CheckLicensePlateServlet</title>");  
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servlet ListContract at " + request.getContextPath() + "</h1>");
+            out.println("<h1>Servlet CheckLicensePlateServlet at " + request.getContextPath () + "</h1>");
             out.println("</body>");
             out.println("</html>");
         }
-    }
+    } 
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
+    /** 
      * Handles the HTTP <code>GET</code> method.
-     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -64,34 +54,12 @@ public class ListContract extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        final String loginFirst = "Bạn cần phải đăng nhập trước!";
-        final String error = "Bạn không có quyền truy cập trang web này!";
-        User u = (User) request.getSession().getAttribute("user");
-        if (u == null) {
-            request.setAttribute("loginFirst", loginFirst);
-            request.getRequestDispatcher("error").forward(request, response);
-        } else {
-            if (u.getRole().equalsIgnoreCase("user") || u.getRole().equalsIgnoreCase("manager")) {
-                request.setAttribute("error", error);
-                request.getRequestDispatcher("error").forward(request, response);
-            } else {
-                String searchQuery = request.getParameter("search");
-                String statusFilter = request.getParameter("status");
+    throws ServletException, IOException {
+        processRequest(request, response);
+    } 
 
-                ContractDAO cd = new ContractDAO();
-                List<Contract> cdtos = cd.searchContracts(searchQuery, statusFilter);
-
-                request.setAttribute("listAll", cdtos);
-
-                request.getRequestDispatcher("listContract.jsp").forward(request, response);
-            }
-        }
-    }
-
-    /**
+    /** 
      * Handles the HTTP <code>POST</code> method.
-     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -99,13 +67,12 @@ public class ListContract extends HttpServlet {
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    throws ServletException, IOException {
         processRequest(request, response);
     }
 
-    /**
+    /** 
      * Returns a short description of the servlet.
-     *
      * @return a String containing servlet description
      */
     @Override
